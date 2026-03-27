f2cb6c61d7c39cb8f4a4acb9ae8937d3b72a2b8c
DominicDiaz <dominicjan@gmail.com>
2026-03-27 20:20:24 +0800

    <feature>(Components) : Created SideBar file

--- src/states/MenuState.jsx
+++ src/states/MenuState.jsx
@@ -1,6 +1,19 @@
-export default function MenuState()
+import TextButton from "../components/TextButton";
+export default function MenuState() 
 {
-    // CODE HERE
-    return (<>
-    </>);
+  const startGame = () => {
+    alert("Game Started!");
+  };
+
+    const openSettings = () => {
+    console.log("Settings opened");
+  };
+
+  return (
+    <>
+      <TextButton text="Start Game" onClick={startGame} />
+      <TextButton text="Settings" onClick={openSettings} />
+      <TextButton text="Exit" onClick={() => console.log("Exit")} />
+    </>
+  );
 }
