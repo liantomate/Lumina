95c8deeb1f0510f561bb0dbb55c5414adcb440ea
Charruzuu <dominicjan02@gmail.com>
2026-03-27 20:26:41 +0800

    <ref>(Components) : test

--- src/states/MenuState.jsx
+++ src/states/MenuState.jsx
@@ -1,19 +1,6 @@
-import TextButton from "../components/TextButton";
-export default function MenuState() 
+export default function MenuState()
 {
-  const startGame = () => {
-    alert("Game Started!");
-  };
-
-    const openSettings = () => {
-    console.log("Settings opened");
-  };
-
-  return (
-    <>
-      <TextButton text="Start Game" onClick={startGame} />
-      <TextButton text="Settings" onClick={openSettings} />
-      <TextButton text="Exit" onClick={() => console.log("Exit")} />
-    </>
-  );
+    // CODE HERE
+    return (<>
+    </>);
 }
