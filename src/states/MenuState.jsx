0d767e3b9a149ae8c5d57253d94ac78b20871811
ailo <tomateliannielz@gmail.com>
2026-03-28 14:45:27 +0800

    <feat>(styles): added a gradient background

--- src/states/MenuState.jsx
+++ src/states/MenuState.jsx
@@ -1,28 +1,14 @@
 import SideBar from "../components/SideBar";
 import IconButton from "../components/IconButton";
 
-export default function MenuState() {
+export default function MenuState()
+{
+    // CODE HERE
+    return (<>
+        <div className="component-background">
 
-  const handleClick = (name) => {
-    console.log("You clicked " + name);
-  };
-
-  return (
-    <>
-      {/* Sidebar */}
-      <SideBar onItemClick={handleClick} />
-
-      {/* Icon buttons */}
-      <div>
-        <IconButton 
-          imagePath="https://avatars.githubusercontent.com/u/69833152?v=4&size=64"
-          onClick={() => handleClick("ICON1")}
-        />
-        <IconButton 
-          imagePath="https://avatars.githubusercontent.com/u/69833152?v=4&size=64"
-          onClick={() => handleClick("ICON2")}
-        />
-      </div>
-    </>
-  );
+        </div>
+        <SideBar className="freeze" />
+        <NavigationBar className="freeze" logoPath={"../../assets/images/logo.png"} title={"Lumina"} progress={"20"}/>
+    </>);
 }
