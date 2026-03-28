9ec2efe4b129732154b0f8518975110fe77dd80c
ailo <tomateliannielz@gmail.com>
2026-03-28 17:22:23 +0800

    <assets>(menu): setup clouds assets for the menu

--- src/states/MenuState.jsx
+++ src/states/MenuState.jsx
@@ -8,8 +8,11 @@ export default function MenuState()
     // CODE HERE
     return (<>
         <div className="component-background">
-
+            {/* <img src="../../assets/images/bg_cloud.png"></img> */}
         </div>
+        {/* <div className="component-islands">
+            <img src="../../assets/images/island_01.png"></img>
+        </div> */}
         <SideBar className="freeze" />
         <NavigationBar className="freeze" logoPath={"../../assets/images/logo.png"} title={"Lumina"} progress={20}/>
     </>);
