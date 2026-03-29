c020f6e2a4ea42532bcaba04c3d7354cd740f785
Natsumi <cjohnawitan@gmail.com>
2026-03-29 16:04:57 +0800

    <feat>(styles): Made NavigationBar responsive

--- src/components/menu_components/MenuNavigationBar.jsx
+++ src/components/menu_components/MenuNavigationBar.jsx
@@ -1,12 +1,12 @@
-import "../../../styles/Component_MenuNavigationBar.css"
+import "../../../styles/Component_MenuNavigationBar.css";
+import ProgressBar from "../ProgressBar";
 
-import ProgressBar from "../ProgressBar"
-
-export default function NavigationBar({ logoPath, title, progress })
-{
-    return (<div className="component-navigation_bar">
-        <img src={logoPath} alt="Logo" />
-        <h1>{title}</h1>
-        <ProgressBar value={progress} />
-    </div>);
+export default function NavigationBar({ logoPath, title, progress }) {
+  return (
+    <div className="component-navigation_bar">
+      <img src={logoPath} alt="Logo" className="nav-logo" />
+      <h1 className="nav-title">{title}</h1>
+      <ProgressBar value={progress} />
+    </div>
+  );
 }
