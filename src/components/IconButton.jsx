242bd09dc2749f625c942a916f7e43d2d90e46c8
ailo <tomateliannielz@gmail.com>
2026-03-31 08:34:50 +0800

    <feat>(sound): implemented sound on IslandButton and IconButton

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,8 +1,19 @@
 import "../../styles/Component_IconButton.css";
 
+import useSoundPlayer from "../hooks/UseSoundPlayerHook";
+import SOUND_TYPES from "../utils/SoundTypes";
+
 export default function IconButton({ imagePath, callback }) {
+    const [ playSFX ] = useSoundPlayer(SOUND_TYPES.EXIT_CLICKED);
+
+    const onButtonClick = () => {
+        console.log("this should play");
+        playSFX();
+        callback();
+    };
+
     return (
-        <button className="component-icon_button" onClick={callback}>
+        <button className="component-icon_button" onClick={onButtonClick}>
             <img src={imagePath} alt="icon" />
         </button>
     );
--- src/utils/SoundTypes.js
+++ src/utils/SoundTypes.js
@@ -2,7 +2,7 @@ const SOUND_TYPES =
 {
     "BUTTON_CLICKED": "../../assets/sfx/button_click_01.mp3",
     "ISLAND_CLICKED": "../../assets/sfx/island_click.mp3",
-    "EXIT_CLICKED": "../../assets/sfx/island_click.mp3",
+    "EXIT_CLICKED": "../../assets/sfx/exit_click.mp3",
 }
 
 export default SOUND_TYPES;
